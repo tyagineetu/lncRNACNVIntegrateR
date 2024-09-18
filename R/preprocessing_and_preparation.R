440027be914c6a3908c323db3ec9faaa986f16a8
Neetu Tyagi <167603005+tyagineetu@users.noreply.github.com>
2024-09-18 17:00:49 +0530

    Update preprocessing_and_preparation.R

--- R/preprocessing_and_preparation.R
+++ R/preprocessing_and_preparation.R
@@ -95,8 +95,6 @@ preprocessing_and_preparation <- function(expr_df, cnv_df, clin_df) {
   fetch_matched_data <- function(vst_data, cnv_data, lncRNA_names, PCG_names) {
     vst_data <- as.data.frame(vst_data)
     cnv_data <- as.data.frame(cnv_data)
-    lncRNA_names <- as.data.frame(lncRNA_names)
-    PCG_names <- as.data.frame(PCG_names)
     lncRNA_expr_data <- vst_data %>%
       select(all_of(intersect(lncRNA_names, colnames(vst_data))))
     
